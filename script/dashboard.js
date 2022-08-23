c2337262fc295e67369e427a39c882dae68e19c0
59114_Chetana <deslechetna1999@gmail.com>
2022-08-23 20:42:19 +0530

    [Chetana] Added: toolbar and sideNav_bar of dashboard

---
+++ script/dashboard.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function () {
+    console.log("connected to js");
+
+    function toggleMenu(){
+        let toggle= document.querySelector('.toggle');
+        let navigation= document.querySelector('.navigation');
+        let navigation=document.querySelector('.main');
+        toggle.classList.toggle('active');
+        navigation.classList.toggle('active');
+        navigation.classList.toggle('active');
+        
+    }
+})
